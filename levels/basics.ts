b879ea2989919a0bad7d6b99e3432fd2676dfed7
lxchurbakov <lxchurbakov@gmail.com>
2023-01-22 20:50:28 +0100

    Make new levels

--- levels/basics.ts
+++ levels/basics.ts
@@ -1,19 +1,224 @@
+// e - empty
+// v - vas3k
+
+const mapping = {
+    ' ': null,
+    'v': 'vas3k',
+    'w': 'wall',
+    'f': 'flag',
+    '3': 't/vas3k',
+    'i': 't/is',
+    'y': 't/you',
+    'g': 't/flag',
+    'n': 't/win',
+    'd': 't/word',
+    'l': 't/wall',
+    'p': 't/push',
+    'o': 'dollar',
+    'r': 't/dollar',
+    's': 't/scared',
+};
+
+const process = (s: string) => {
+    return s.split('').reduce((acc, c) => {
+        if (c === '\n') {
+            acc.push([]);
+        }
+
+        acc[acc.length - 1].push(mapping[c]);
+
+        return acc;        
+    }, [[]] as any[]);
+};
+
+
 export default [
+    // A level to make people sense game
     [
-        [ , , , 'vas3k', , 'flag', 'bug'],
-        [ , , , 'wall'],
+        ,,,,,
+        [,,,,,'vas3k',,,,'flag'],
         ,
-        [, 't/vas3k', 't/is', 't/you'],
+        [,,,,,'t/vas3k', 't/is', 't/you'],
+        [,,,,,'t/flag', 't/is', 't/win'],
+    ],
+    // A level to make people move walls
+    [
+        ,,,,,
+        [,,,,,'vas3k',,,,      , 'wall'],
+        [,,,,,       ,,,,'wall', 'flag', 'wall'],
+        [,,,,,       ,,,,      , 'wall'],
         ,
-        [, 't/flag', 't/is', 't/win'],
-        ,
-        [, 't/wall', 't/is', 't/push'],
-        ,
-        [, 't/word', 't/is', 't/push'],
-        [, 't/bug', 't/kill', 't/vas3k'],
-        [, 't/bug', 't/is', 't/angry'],
-        [, 'wota', 'wota'],
-    ]
+        [,,,,,'t/vas3k', 't/is', 't/you'],
+        [,,,,,'t/flag', 't/is', 't/win'],
+        [,,,,,'t/wall', 't/is', 't/push'],
+    ],
+    // A level to make people move walls
+    process(` 
+
+
+            v  www
+               wfw
+               www
+        lg
+        3iy dip 
+         n
+    `),
+    // A level to introduce dollar
+    process(`
+
+
+        v o
+        3iy rin
+    `),
+    // Dollar is scared
+    process(`
+               w 
+          v    w o
+               w   
+         r   r   
+        3iy dip
+         s   n
+    `)
+    // Bugs?
+    // Dollar is scared
+    // Water kills vas3k
+    // doors and keys
+    // transform to many walls
+    // A level to make people
+    // move walls
+    // water
+    // bug
+    // door
+    // 
+
+    // [
+    //     [ , , , 'vas3k', , 'flag', 'bug'],
+    //     [ , , , 'wall'],
+    //     ,
+    //     [, 't/vas3k', 't/is', 't/you'],
+    //     ,
+    //     [, 't/flag', 't/is', 't/win'],
+    //     ,
+    //     [, 't/wall', 't/is', 't/push'],
+    //     [,,,,,'bug'],
+    //     ,
+    //     [, 't/word', 't/is', 't/push'],
+    //     [, 't/wota', 't/kill', 't/vas3k'],
+    //     [, 't/bug', 't/is', 't/scared'],
+    //     [, 'wota', 'wota'],
+    // ],
+    // [
+    //     [ , , , 'vas3k', , 'flag', 'bug'],
+    //     [ , , , 'wall'],
+    //     ,
+    //     [, 't/vas3k', 't/is', 't/you'],
+    //     ,
+    //     [, 't/flag', 't/is', 't/win'],
+    //     ,
+    //     [, 't/wall', 't/is', 't/push'],
+    //     [,,,,,'bug'],
+    //     ,
+    //     [, 't/word', 't/is', 't/push'],
+    //     [, 't/wota', 't/kill', 't/vas3k'],
+    //     [, 't/bug', 't/is', 't/scared'],
+    //     [, 'wota', 'wota'],
+    // ],
+    // [
+    //     [ , , , 'vas3k', , 'flag', 'bug'],
+    //     [ , , , 'wall'],
+    //     ,
+    //     [, 't/vas3k', 't/is', 't/you'],
+    //     ,
+    //     [, 't/flag', 't/is', 't/win'],
+    //     ,
+    //     [, 't/wall', 't/is', 't/push'],
+    //     [,,,,,'bug'],
+    //     ,
+    //     [, 't/word', 't/is', 't/push'],
+    //     [, 't/wota', 't/kill', 't/vas3k'],
+    //     [, 't/bug', 't/is', 't/scared'],
+    //     [, 'wota', 'wota'],
+    // ],
+    // [
+    //     [ , , , 'vas3k', , 'flag', 'bug'],
+    //     [ , , , 'wall'],
+    //     ,
+    //     [, 't/vas3k', 't/is', 't/you'],
+    //     ,
+    //     [, 't/flag', 't/is', 't/win'],
+    //     ,
+    //     [, 't/wall', 't/is', 't/push'],
+    //     [,,,,,'bug'],
+    //     ,
+    //     [, 't/word', 't/is', 't/push'],
+    //     [, 't/wota', 't/kill', 't/vas3k'],
+    //     [, 't/bug', 't/is', 't/scared'],
+    //     [, 'wota', 'wota'],
+    // ],
+    // [
+    //     [ , , , 'vas3k', , 'flag', 'bug'],
+    //     [ , , , 'wall'],
+    //     ,
+    //     [, 't/vas3k', 't/is', 't/you'],
+    //     ,
+    //     [, 't/flag', 't/is', 't/win'],
+    //     ,
+    //     [, 't/wall', 't/is', 't/push'],
+    //     [,,,,,'bug'],
+    //     ,
+    //     [, 't/word', 't/is', 't/push'],
+    //     [, 't/wota', 't/kill', 't/vas3k'],
+    //     [, 't/bug', 't/is', 't/scared'],
+    //     [, 'wota', 'wota'],
+    // ],
+    // [
+    //     [ , , , 'vas3k', , 'flag', 'bug'],
+    //     [ , , , 'wall'],
+    //     ,
+    //     [, 't/vas3k', 't/is', 't/you'],
+    //     ,
+    //     [, 't/flag', 't/is', 't/win'],
+    //     ,
+    //     [, 't/wall', 't/is', 't/push'],
+    //     [,,,,,'bug'],
+    //     ,
+    //     [, 't/word', 't/is', 't/push'],
+    //     [, 't/wota', 't/kill', 't/vas3k'],
+    //     [, 't/bug', 't/is', 't/scared'],
+    //     [, 'wota', 'wota'],
+    // ],
+    // [
+    //     [ , , , 'vas3k', , 'flag', 'bug'],
+    //     [ , , , 'wall'],
+    //     ,
+    //     [, 't/vas3k', 't/is', 't/you'],
+    //     ,
+    //     [, 't/flag', 't/is', 't/win'],
+    //     ,
+    //     [, 't/wall', 't/is', 't/push'],
+    //     [,,,,,'bug'],
+    //     ,
+    //     [, 't/word', 't/is', 't/push'],
+    //     [, 't/wota', 't/kill', 't/vas3k'],
+    //     [, 't/bug', 't/is', 't/scared'],
+    //     [, 'wota', 'wota'],
+    // ],
+    // [
+    //     [ , , , 'vas3k', , 'flag', 'bug'],
+    //     [ , , , 'wall'],
+    //     ,
+    //     [, 't/vas3k', 't/is', 't/you'],
+    //     ,
+    //     [, 't/flag', 't/is', 't/win'],
+    //     ,
+    //     [, 't/wall', 't/is', 't/push'],
+    //     [,,,,,'bug'],
+    //     ,
+    //     [, 't/word', 't/is', 't/push'],
+    //     [, 't/wota', 't/kill', 't/vas3k'],
+    //     [, 't/bug', 't/is', 't/scared'],
+    //     [, 'wota', 'wota'],
+    // ],
 ];
 
 // constructor (private canvas: Canvas, private events: Events) {
