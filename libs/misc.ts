3cdd3931a4fdb292927f290a708ec07769ffc3b6
lxchurbakov <lxchurbakov@gmail.com>
2023-01-22 12:16:32 +0100

    Add Point related functions

--- libs/misc.ts
+++ libs/misc.ts
@@ -1,1 +1,14 @@
 export type Point = { x: number, y: number };
+
+export const same = (a: Point, b: Point) => 
+    a.x === b.x && a.y === b.y;
+
+export const add = (a: Point, b: Point) => 
+    ({ x: a.x + b.x, y: a.y + b.y });
+
+export const distance = (a: Point, b: Point) => {
+    return Math.sqrt(
+        Math.pow(a.x - b.x, 2) + 
+        Math.pow(a.y - b.y, 2)
+    );
+};
